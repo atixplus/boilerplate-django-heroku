f49dba42e5ac636022add4a93262beb2d49cb249
Javier <bichocj@gmail.com>
2020-06-29 13:19:56 -0500

    added admin

--- backend/AppWeb/web/admin.py
+++ backend/AppWeb/web/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import State
 # Register your models here.
-admin.site.register(State)
+class StateAdmin(admin.ModelAdmin):
+  list_display = ('user', 'event', 'timestamp')
+  
+admin.site.register(State, StateAdmin)
